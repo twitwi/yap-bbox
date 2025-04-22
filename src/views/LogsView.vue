441f97b0a1bbac834d10748aba62f3589f8b2e13
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-04-22 23:12:47 +0200

    Nicer log view

--- src/views/LogsView.vue
+++ src/views/LogsView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+import router from '@/router'
 import { useMainStore } from '@/stores/simple'
 import { Activity, DUMMY_ACTIVITY, DUMMY_LOG, Log } from '@/typing'
 import { useNow } from '@vueuse/core'
 import { computed } from 'vue'
 const main = useMainStore()
 
-const now = useNow({ interval: 500, controls: true })
+const now = useNow({ interval: 500 })
 
 function showTime(t) {
   return new Date(t).toISOString().substring(11, 19)
@@ -24,7 +25,7 @@ const lastActivity = computed(() => {
 const lastLogTime = computed(() => {
   const log = lastLog.value
   if (log.end.length === 0) {
-    const live = now.now.value - log.start
+    const live = now.value.getTime() - log.start
     return showTime(live)
   }
   const duration = Math.min(...log.end) - log.start
@@ -43,12 +44,30 @@ const beforeLastActivity = computed(() => {
 const beforeLastLogTime = computed(() => {
   const log = beforeLastLog.value
   if (log.end.length === 0) {
-    const live = now.now.value - log.start
+    const live = now.value.getTime() - log.start
     return showTime(live)
   }
   const duration = Math.min(...log.end) - log.start
   return showTime(duration)
 })
+
+function clickLastLog() {
+  if (lastLog.value.end.length == 0) {
+    lastLog.value.end.push(Date.now() - 500)
+  } else {
+    clickActivity(lastActivity.value)
+  }
+}
+function clickBeforeLastLog() {
+  if (beforeLastLog.value.end.length == 0) {
+    beforeLastLog.value.end.push(Date.now() - 500)
+  } else {
+    clickActivity(beforeLastActivity.value)
+  }
+}
+function edit(log: Log) {
+  router.push({ name: 'log', params: { logIndex: main.logs.indexOf(log) } })
+}
 
 function clickActivity(a: Activity) {
   if (lastLog.value.end.length == 0) {
@@ -67,16 +86,22 @@ function clickActivity(a: Activity) {
 <template>
   <h3>Logging</h3>
 
-  <div class="log last-log" :style="{ ['--col']: lastActivity.color }">
-    <div>Current Log</div>
-    <div>{{ lastLogTime }}</div>
-    <p>{{ lastLog.comment }}</p>
+  <div :class="{log:true, 'last-log': true, running: lastLog.end.length == 0}" :style="{ ['--col']: lastActivity.color }">
+    <div class="time" @click="clickLastLog()">{{ lastLogTime }}</div>
+    <div class="activity">
+      <div class="name">{{ lastActivity.name }}</div>
+      <div class="comment">{{ lastLog.comment }}</div>
+    </div>
+    <div class="edit" @click="edit(lastLog)">edit</div>
   </div>
 
-  <div class="log before-last-log" :style="{ ['--col']: beforeLastActivity.color }">
-    <div>Previous Log</div>
-    <div>{{ beforeLastLogTime }}</div>
-    <p>{{ beforeLastLog.comment }}</p>
+  <div :class="{log:true, 'beforelast-log': true, running: beforeLastLog.end.length == 0}" :style="{ ['--col']: beforeLastActivity.color }">
+    <div class="time" @click="clickBeforeLastLog()">{{ beforeLastLogTime }}</div>
+    <div class="activity">
+      <div class="name">{{ beforeLastActivity.name }}</div>
+      <div class="comment">{{ beforeLastLog.comment }}</div>
+    </div>
+    <div class="edit" @click="edit(beforeLastLog)">edit</div>
   </div>
 
   <div class="pick-activities">
@@ -92,7 +117,51 @@ function clickActivity(a: Activity) {
 
 <style>
 .log {
-  border: 2px solid grey;
+  background: lightgrey;
+  .time {
+    text-align: center;
+    font-size: 3em;
+    .log.running & {
+      color: #400;
+      font-weight: lighter;
+    }
+    .beforelast-log & {
+      color: #444;
+      display: inline-block;
+      width: 20%;
+      font-size: 2em;
+    }
+  }
+  .activity {
+    display: inline-block;
+    width: 80%;
+    .beforelast-log & {
+      width: 60%;
+    }
+    min-height: 3em;
+    margin: 0 1em 1em;
+    border: 1em solid var(--col);
+    border-width: 0 1em;
+    background: black;
+    color: white;
+    .name {
+      text-align: center;
+    }
+    .comment {
+      color: #f99;
+    }
+  }
+  .edit {
+    display: inline-block;
+    width: calc(20% - 4em);
+    margin: 0 1em;
+    padding: .5em;
+    text-align: center;
+    cursor: pointer;
+    background: #444;
+    color: white;
+  }
+  margin: 1em;
 }
 .pick-activities {
   display: flex;
