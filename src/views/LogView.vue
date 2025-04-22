570da79b856d3115ab29fa572911466f9c830b31
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-04-23 00:12:32 +0200

    Edit with naiveui

--- src/views/LogView.vue
+++ src/views/LogView.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { NButton, NDatePicker, NForm, NFormItem, NInput, NSelect, NSpace } from 'naive-ui'
+
 const props = defineProps({
   logIndex: {
     type: Number,
@@ -16,6 +18,14 @@ const log = computed(() => {
 const activity = computed(() => {
   return main.activities.find((a) => a.id === log.value.activity)
 })
+const activityOptions = computed(() => {
+  return main.activities.map((a) => ({
+    label: a.name,
+    value: a.id,
+    key: a.id,
+    color: a.color,
+  }))
+})
 
 function promptConfirmDelete() {
   const confirm = window.confirm('Are you sure you want to delete this log?')
@@ -30,12 +40,32 @@ function promptConfirmDelete() {
   <button @click="router.go(-1)">Back</button>
 
   <template v-if="log">
-    <select v-model="log.activity">
-      <option v-for="a in main.activities" :key="a.id" :value="a.id" :style="{['--col']: a.color}">{{ a.name }}</option>
-    </select>
-    <input type="text" v-model="log.comment" placeholder="Comment" />
-    <button @click="promptConfirmDelete()">Delete</button>
-    <pre>{{ JSON.stringify(log, null, 2) }}</pre>
+    <NForm>
+      <NFormItem label="Activity">
+        <div :style="{'--col': activity.color}" class="activity-color"></div>
+        <NSelect v-model:value="log.activity" :options="activityOptions" />
+      </NFormItem>
+      <NFormItem label="Start">
+        <NDatePicker v-model:value="log.start" type="datetime" />
+      </NFormItem>
+      <NFormItem label="End">
+        <NDatePicker v-for="e,i in log.end" :key="i" v-model:value="log.end[i]" type="datetime" />
+        <div style="width: 3em"></div>
+        <NButton v-if="log.end.length == 0" @click="log.end.push(Date.now())">end now</NButton>
+        <NButton v-else @click="log.end.splice(0, log.end.length)" type="error" tertiary>clear end(s)</NButton>
+      </NFormItem>
+      <NFormItem label="Comment">
+        <NInput type="text" v-model="log.comment" placeholder="Comment" />
+      </NFormItem>
+    </NForm>
+    <NButton @click="promptConfirmDelete" type="error">Delete</NButton>
   </template>
 </template>
 
+<style>
+.activity-color {
+  width: 1em;
+  height: 2em;
+  background-color: var(--col);
+}
+</style>
