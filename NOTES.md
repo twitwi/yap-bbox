76adc966b576fb75e3dba754f90c50be5cd2ab7e
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-07-22 02:27:55 +0200

    Use wrapped builtin datetime-local selector

--- NOTES.md
+++ NOTES.md
@@ -9,8 +9,8 @@ pnpm  build --base /2025-bboxy/
 
 - [ ] Esc to go back on desktop
 - [x] bigger menu bar
-- [ ] better date-time selection
-- [ ] options: no-history mode to have "back" quit the app
+- [x] better date-time selection
+- [ ] options: no-history mode to have "back" quit the app (seems ok?)
 - [x] focus the comment field
 - [ ] tentative rework with action to enable predictive actions (or based on state?), e.g. suggest adding a typical comment etc
 
--- src/views/LogView.vue
+++ src/views/LogView.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 
 import router from '@/router'
 import { useMainStore } from '@/stores/simple'
-import { computed, ref, watch } from 'vue'
+import { computed, ref, watch, type Ref } from 'vue'
 import { DUMMY_ACTIVITY } from '@/typing'
 import BackButton from '@/components/BackButton.vue'
 const main = useMainStore()
@@ -44,6 +44,20 @@ watch(focused, (el) => {
   }
 }, { immediate: true })
 
+const wrapDate = <K extends number|string>(k: K) => (it: () => Record<K, number>) => computed({
+  get: () => new Date(it()[k] - new Date().getTimezoneOffset() * 60000).toISOString().replace(/Z/, ''),
+  set: (value: string) => {
+    const date = new Date(value)
+    if (!isNaN(date.getTime())) {
+      it()[k] = date.getTime()
+    }
+  },
+})
+const wrapStart = wrapDate('start')(() => log.value)
+const wrapEnd = computed(() => {
+  return log.value.end.map((e, i) => wrapDate(i)(() => log.value.end))
+})
+
 </script>
 
 <template>
@@ -56,10 +70,10 @@ watch(focused, (el) => {
         <NSelect v-model:value="log.activity" :options="activityOptions" />
       </NFormItem>
       <NFormItem label="Start">
-        <NDatePicker v-model:value="log.start" type="datetime" />
+        <input type="datetime-local" v-model="wrapStart" />
       </NFormItem>
       <NFormItem label="End">
-        <NDatePicker v-for="e,i in log.end" :key="i" v-model:value="log.end[i]" type="datetime" />
+        <input v-for="(e,i) in log.end" :key="i" type="datetime-local" v-model="wrapEnd[i].value" />
         <div style="width: 3em"></div>
         <NButton v-if="log.end.length == 0" @click="log.end.push(Date.now())">end now</NButton>
         <NButton v-else @click="log.end.splice(0, log.end.length)" type="error" tertiary>clear end(s)</NButton>
